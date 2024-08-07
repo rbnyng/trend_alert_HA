c643795c412f200f7b9167f148353088b6a20bc5
Robin Young <59581322+rbnyng@users.noreply.github.com>
2024-08-07 11:10:06 +0800

    Update alert.py

--- alert.py
+++ alert.py
@@ -85,7 +85,7 @@ if current_state != last_state:
     message['Subject'] = 'QQQ Market Condition Change Alert'
     message['From'] = sender_email
     message['To'] = receiver_email
-    body = 'The QQQ market condition has changed.\n' + current_state + '\n[Check repository](https://github.com/rbnyng/trend_alert)'
+    body = 'The QQQ market condition has changed.\n' + current_state + '\n[Check repository](https://github.com/rbnyng/trend_alert_HA)'
     message.attach(MIMEText(body, 'plain'))
 
     # Send the email
