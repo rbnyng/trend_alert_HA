01609adf17f87896c5068ec7c7b6166238cb46df
Robin Young <59581322+rbnyng@users.noreply.github.com>
2024-12-10 21:35:03 +0000

    Update apply_labels.py

--- helpers/apply_labels.py
+++ helpers/apply_labels.py
@@ -6,38 +6,38 @@ def apply_labels(df):
     df_copy['Alert'] = ''
     df_copy['Market_Condition'] = ''  
     
-    for i in range(len(df_copy)):
-        close = df_copy.loc[i, 'haClose']
-        ema_50 = df_copy.loc[i, 'EMA_50']
-        ema_200 = df_copy.loc[i, 'EMA_200']
-        ha_color = df_copy.loc[i, 'HA_Color']
-        hma_color = df_copy.loc[i, 'HMA_color']
-        
-        # Determine market condition
-        if close < ema_50 and close < ema_200:
-            df_copy.loc[i, 'Market_Condition'] = 'Bear'
-            # Bear market conditions
-            if ha_color == 'green' and hma_color in ['green', 'dark_green']:
-                df_copy.loc[i, 'Alert'] = 'Long Alert'
-            elif hma_color in ['dark_orange', 'red'] or ha_color == 'red':
-                df_copy.loc[i, 'Alert'] = 'Sell Alert'
-                
-        elif close > ema_50 and close > ema_200:
-            df_copy.loc[i, 'Market_Condition'] = 'Bull'
-            # Bull market conditions
-            if ha_color == 'green' and hma_color in ['green', 'dark_green']:
-                df_copy.loc[i, 'Alert'] = 'Long Alert'
-            elif hma_color == 'red' or ha_color == 'red':
-                df_copy.loc[i, 'Alert'] = 'Sell Alert'
-            elif hma_color == 'dark_orange':
-                df_copy.loc[i, 'Alert'] = 'Orange HMA Alert'
-                
-        else:
-            df_copy.loc[i, 'Market_Condition'] = 'Sideways'
-            # Sideways market conditions
-            if ha_color == 'green' and hma_color in ['green', 'dark_green']:
-                df_copy.loc[i, 'Alert'] = 'Long Alert'
-            elif hma_color in ['dark_orange', 'red'] or ha_color == 'red':
-                df_copy.loc[i, 'Alert'] = 'Sell Alert'
+    # Determine market condition (Vectorized)
+    bear_condition = (df_copy['haClose'] < df_copy['EMA_50']) & (df_copy['haClose'] < df_copy['EMA_200'])
+    bull_condition = (df_copy['haClose'] > df_copy['EMA_50']) & (df_copy['haClose'] > df_copy['EMA_200'])
+    sideways_condition = ~bear_condition & ~bull_condition  # Not bear and not bull
+
+    df_copy.loc[bear_condition, 'Market_Condition'] = 'Bear'
+    df_copy.loc[bull_condition, 'Market_Condition'] = 'Bull'
+    df_copy.loc[sideways_condition, 'Market_Condition'] = 'Sideways'
+
+    # --- Apply alerts based on market condition (Vectorized) ---
+
+    # Bear Market Alerts
+    long_alert_bear = bear_condition & (df_copy['HA_Color'] == 'green') & df_copy['HMA_color'].isin(['green', 'dark_green'])
+    sell_alert_bear = bear_condition & (df_copy['HMA_color'].isin(['dark_orange', 'red']) | (df_copy['HA_Color'] == 'red'))
+
+    df_copy.loc[long_alert_bear, 'Alert'] = 'Long Alert'
+    df_copy.loc[sell_alert_bear, 'Alert'] = 'Sell Alert'
+
+    # Bull Market Alerts
+    long_alert_bull = bull_condition & (df_copy['HA_Color'] == 'green') & df_copy['HMA_color'].isin(['green', 'dark_green'])
+    sell_alert_bull = bull_condition & ((df_copy['HMA_color'] == 'red') | (df_copy['HA_Color'] == 'red'))
+    orange_hma_alert_bull = bull_condition & (df_copy['HMA_color'] == 'dark_orange')
+
+    df_copy.loc[long_alert_bull, 'Alert'] = 'Long Alert'
+    df_copy.loc[sell_alert_bull, 'Alert'] = 'Sell Alert'
+    df_copy.loc[orange_hma_alert_bull, 'Alert'] = 'Orange HMA Alert'
+    
+    # Sideways Market Alerts
+    long_alert_sideways = sideways_condition & (df_copy['HA_Color'] == 'green') & df_copy['HMA_color'].isin(['green', 'dark_green'])
+    sell_alert_sideways = sideways_condition & (df_copy['HMA_color'].isin(['dark_orange', 'red']) | (df_copy['HA_Color'] == 'red'))
+
+    df_copy.loc[long_alert_sideways, 'Alert'] = 'Long Alert'
+    df_copy.loc[sell_alert_sideways, 'Alert'] = 'Sell Alert'
 
     return df_copy
